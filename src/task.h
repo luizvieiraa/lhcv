d13f89eca9424b5d85be198a474b61286009198f
Luiz Vieira <lhcv@cesar.school>
2026-08-19 10:07:32 -0300

    task.h

---
+++ src/task.h
@@ -0,0 +1,23 @@
+#ifndef TASK_H
+#define TASK_H
+
+#define MAX_TASKS 50
+#define MAX_ARGS 20
+
+typedef struct {
+
+    char nome[50];
+    char programa[100];
+    char *argumentos[MAX_ARGS];
+    int quantidade_argumentos;
+
+}Task;
+
+void cadastrar_task(
+    Task tarefas[],
+    int *quantidade,
+    char *tokens[],
+    int quantidade_tokens
+);
+
+#endif
